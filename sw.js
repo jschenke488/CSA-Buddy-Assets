145da4550c131addc981968757533b1bab6f8d0d
Jay <44783760+jschenke488@users.noreply.github.com>
2026-09-17 20:48:13 -0500

    Update 2.0.8: Add guides tab

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this on every deploy so clients pick up fresh assets.
-const CACHE_VERSION = 'csa-buddy-sw-v9'
+const CACHE_VERSION = 'csa-buddy-sw-v10'
 
 const PRECACHE_URLS = [
     '/aboutpage.html',
